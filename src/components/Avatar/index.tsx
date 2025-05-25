b4a3973bd689fb2bc7254d0ce8411c924014aef2
fabricioLJdev <fabricioj516gb@outlook.com>
2025-05-25 13:48:06 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="http://github.com/fabricioLJdev.png" />
+
+export default Avatar
--- src/containers/Projetos/index.tsx
+++ src/containers/Projetos/index.tsx
@@ -10,6 +10,30 @@ const Projetos = () => (
       <li>
         <Projeto />
       </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
     </Lista>
   </section>
 )
